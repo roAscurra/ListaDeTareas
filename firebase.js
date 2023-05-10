5f25b8cf0ce9211fa3c46a4be99d27acc2e22a8a
roAscurra <rominaascurra4887@gmail.com>
2023-05-10 15:27:16 -0300

    Mostrando lista de tareas

--- firebase.js
+++ firebase.js
@@ -20,6 +20,39 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
+const lista = document.getElementById('listaTareas');
+const renderList = (doc) => {
+  let li = document.createElement('li');
+  li.className = 'collection-item';
+  li.setAttribute('data-id', doc.id);
+
+  let div = document.createElement('div');
+  let titulo = document.createElement('span');
+  titulo.textContent = doc.data().Titulo;
+
+  let enlace = document.createElement('a');
+  enlace.href = '#modal1';
+  enlace.className = 'modal-trigger secondary-content';
+
+  let editBtn = document.createElement('i');
+  editBtn.className = 'material-icons';
+  editBtn.innerText = 'edit';
+
+  let deleteBtn = document.createElement('i');
+  deleteBtn.className = 'material-icons secondary-content';
+  deleteBtn.innerText = 'delete';
+
+  enlace.appendChild(editBtn);
+  div.appendChild(titulo);
+  div.appendChild(deleteBtn);
+  div.appendChild(enlace);
+  li.appendChild(div);
+
+  deleteBtn.addEventListener('click', e => {
+    console.log('delete');
+  })
+  lista.append(li);
+}
 export const getCambios = () => {
   const querys = query(collection(db, 'tareas'), orderBy('Titulo'));
   const unsubscribe = onSnapshot(querys, snapshot => {
@@ -32,7 +65,7 @@ export const getCambios = () => {
     console.log(cambios);
     cambios.forEach(cambio => {
       if(cambio.type == 'added'){
-        console.log(cambio.doc.data());
+        renderList(cambio.doc)
       }else if(cambio.type == 'removed'){
         console.log('Eliminado');
       }else if(cambio.type == 'modified'){
