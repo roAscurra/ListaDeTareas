1e2cf29bdf6ccfe8ea3a99b75d4832ac4d5dcfbb
roAscurra <rominaascurra4887@gmail.com>
2023-05-24 17:35:13 -0300

    Llamando a la funcion agregarTarea
    Al cargar la pagina se obtiene el nombre del titulo ingresado y se le pasa como parametro a la funcion

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-import {getCambios } from "./firebase.js";
+import {getCambios, agregarTarea } from "./firebase.js";
 window.addEventListener('DOMContentLoaded', async () =>{
        getCambios();
+       document.getElementById('agregarBtn').addEventListener('click', (e) => {
+              e.preventDefault()
+              const tituloInput = document.getElementById('autocomplete-input');
+              const titulo = tituloInput.value;
+            
+              if (titulo) {
+                agregarTarea(titulo);
+                tituloInput.value='';
+              }else{
+                console.log('No ingresó un titulo')
+              }
+            });
 })
